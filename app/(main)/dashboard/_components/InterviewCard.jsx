323953dc3cec7a98db483169327e48df91b104e3
azpradipta <aryazakypradipta@gmail.com>
2025-11-26 01:11:56 +0800

    feat: Display Previously Created Interviews

---
+++ app/(main)/dashboard/_components/InterviewCard.jsx
@@ -0,0 +1,35 @@
+import { Button } from '@/components/ui/button'
+import { Copy, Send } from 'lucide-react'
+import moment from 'moment'
+import React from 'react'
+import { toast } from 'sonner'
+
+function InterviewCard({ interview }) {
+    const url = process.env.NEXT_PUBLIC_HOST_URL + "/" + interview?.interview_id
+
+    const copyLink = () => {
+        navigator.clipboard.writeText(url);
+        toast('Copied')
+    }
+
+    const onSend = () => {
+        window.location.href="mailto:11231013@student.itk.ac.id?subject=Recruitbot Interview Link & body=Interview Link:" + url
+    }
+
+    return (
+        <div className='p-5 bg-white rounded-lg border'>
+            <div className='flex items-center justify-between'>
+                <div className='h-[40px] w-[40px] bg-primary rounded-full'></div>
+                <h2 className='text-sm'>{moment(interview?.created_at).format('DD MMM yyy')}</h2>
+            </div>
+                <h2 className='mt-3 font-bold text-lg'>{interview?.jobPosition}</h2>
+                <h2 className='mt-2'>{interview?.duration}</h2>
+            <div className='grid grid-cols-2 gap-3 mt-5'>
+                <Button variant='outline' className={'w-full'} onClick={copyLink}> <Copy /> Copy Link</Button>
+                <Button className={'w-full'} onClick={onSend}> <Send /> Send</Button>
+            </div>
+        </div>
+    )
+}
+
+export default InterviewCard
