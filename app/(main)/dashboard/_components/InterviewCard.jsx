1e46020c254f9cc32af49220f5d20a19db5cb658
azpradipta <aryazakypradipta@gmail.com>
2025-11-27 09:45:35 +0800

    feat: Display Scheduled Interviews

--- app/(main)/dashboard/_components/InterviewCard.jsx
+++ app/(main)/dashboard/_components/InterviewCard.jsx
@@ -1,10 +1,10 @@
 import { Button } from '@/components/ui/button'
-import { Briefcase, Copy, Send } from 'lucide-react'
+import { ArrowRight, Briefcase, Copy, Send } from 'lucide-react'
 import moment from 'moment'
 import React from 'react'
 import { toast } from 'sonner'
 
-function InterviewCard({ interview }) {
+function InterviewCard({ interview, viewDetail=false }) {
     const url = process.env.NEXT_PUBLIC_HOST_URL + "/" + interview?.interview_id
 
     const copyLink = () => {
@@ -25,11 +25,16 @@ function InterviewCard({ interview }) {
                 <h2 className='text-sm'>{moment(interview?.created_at).format('DD MMM yyy')}</h2>
             </div>
                 <h2 className='mt-3 font-bold text-lg'>{interview?.jobPosition}</h2>
-                <h2 className='mt-2'>{interview?.duration}</h2>
-            <div className='grid grid-cols-2 gap-3 mt-5'>
+                <h2 className='mt-2 flex justify-between text-gray-500'>{interview?.duration}
+                    <span className='text-green-600'>{interview['interview-feedback']?.length} Candidates</span>
+                </h2>
+            {!viewDetail? <div className='grid grid-cols-2 gap-3 mt-5'>
                 <Button variant='outline' className={'w-full'} onClick={copyLink}> <Copy /> Copy Link</Button>
                 <Button className={'w-full'} onClick={onSend}> <Send /> Send</Button>
             </div>
+            :
+            <Button className="mt-5 w-full" variant="outline">View Detail <ArrowRight /></Button>
+            }
         </div>
     )
 }
